c032641395ee1776d583f6a2505e530cb3cca566
Grant Heffernan <heffergm@gmail.com>
2013-05-25 15:35:10 -0400

    1

--- hello.js
+++ hello.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer(function (request, response) {
       response.writeHead(200, {'Content-Type': 'text/plain'});
-          response.end('JUST TESTING\n');
+          response.end('PICKLES PICKLES 123, PICKLES PICKLES CANT YOU SEE\n');
 }).listen(8080);
 
 console.log('Server started')
